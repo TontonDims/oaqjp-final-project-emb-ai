5e9b2ff766b73c58b636c0e5035ee0565e186d5c
Tes Too <testoo@email.com>
2026-07-20 09:54:01 -0400

    Correcting error

--- server.py
+++ server.py
@@ -4,18 +4,18 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector") 
-    def emotion_detector(): 
-    # Récupérer le texte à analyser depuis les arguments de la requête 
-    text_to_analyze = request.args.get('textToAnalyze')
+def emotion_detector(): 
+# Récupérer le texte à analyser depuis les arguments de la requête 
+text_to_analyze = request.args.get('textToAnalyze')
 
-    # Passer le texte à la fonction emotion_detector et stocker la réponse 
-    response = emotion_detector(text_to_analyze)
+# Passer le texte à la fonction emotion_detector et stocker la réponse 
+response = emotion_detector(text_to_analyze)
 
-    # Extraire l'étiquette et le score de la réponse 
-    emotion = response['emotion'] score = response['score']
+# Extraire l'étiquette et le score de la réponse 
+emotion = response['emotion'] score = response['score']
 
-    # Retourner une chaîne formatée avec l'étiquette des émotions et les scores 
-    return "Pour l'énoncé donnée la réponse du système est  {} l'émotion dominante est {}.".format(emotion.split('_')[1], score)
+# Retourner une chaîne formatée avec l'étiquette des émotions et les scores 
+return "Pour l'énoncé donnée la réponse du système est  {} l'émotion dominante est {}.".format(emotion.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
